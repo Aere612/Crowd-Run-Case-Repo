4b90a5e984a124dc9cdfb8e03322c27a9dc722ed
@gncrysr <gncrysr@gmail.com>
2022-12-31 15:25:44 +0300

    player movement

---
+++ Crowd Run Case/Assets/Scripts/Movement.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+
+    [Header("Elements")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] private float maxDisplacement = 0.2f;
+    [SerializeField] private float maxPositionX = 2f;
+    private Vector2 _anchorPosition;
+
+    Camera cam;
+    Vector3 camOffset;
+
+    //CONSTRAINTS
+    bool canForward;
+    bool canSide, canMove;
+    bool isInit = false;
+    public bool ComForward { get { return canForward; } set { canForward = value; } }
+    public bool Canside { get { return canSide; } set { canSide = value; } }
+    public bool CanMove { get { return canMove; } set { canMove = value; } }
+    private void Start() {
+        Initialize();
+    }
+    public void Initialize() {
+        canForward = true;
+        canSide = true;
+        canMove = true;
+
+        cam = Camera.main;
+        camOffset = cam.transform.position - this.transform.position;
+
+        isInit= true;
+    }
+    private void Update() {
+        if (!isInit || !canMove)
+            return;
+        MoveForward();
+        MoveSide();
+    }
+    private void LateUpdate() {
+        if(!isInit || !CanMove)
+            return;
+        MoveCam();
+    }
+    #region MOVE
+    void MoveForward() {
+        if (!canForward)
+            return;
+        this.transform.position += Vector3.forward * (moveSpeed * Time.deltaTime);
+    }
+    void MoveSide() {
+        if (!canSide)
+            return;
+        var inputX = GetInput();
+
+        var displacementX = GetDisplacement(inputX);
+
+        displacementX = SmoothOutDisplacement(displacementX);
+
+        var newPosition = GetNewLocalPosition(displacementX);
+
+        newPosition = GetLimitedLocalPosition(newPosition);
+
+        this.transform.localPosition = newPosition;
+    }
+    private Vector3 GetLimitedLocalPosition(Vector3 position) {
+        position.x = Mathf.Clamp(position.x, -maxPositionX, maxPositionX);
+        return position;
+    }
+    private Vector3 GetNewLocalPosition(float displacementX) {
+        var lastPosition = transform.localPosition;
+        var newPositionX = lastPosition.x + displacementX;
+        var newPosition = new Vector3(newPositionX, lastPosition.y, lastPosition.z);
+        return newPosition;
+    }
+    private float GetInput() {
+        var inputX = 0f;
+        if (Input.GetMouseButtonDown(0)) {
+            _anchorPosition = Input.mousePosition;
+        } else if (Input.GetMouseButton(0)) {
+            inputX = (Input.mousePosition.x - _anchorPosition.x);
+            _anchorPosition = Input.mousePosition;
+        }
+        return inputX;
+    }
+    private float GetDisplacement(float inputX) {
+        var displacementX = 0f;
+        displacementX = inputX * Time.deltaTime;
+        return displacementX;
+    }
+    private float SmoothOutDisplacement(float displacementX) {
+        return Mathf.Clamp(displacementX, -maxDisplacement, maxDisplacement);
+    }
+    #endregion
+    #region CAMERA
+    void MoveCam() {
+        cam.transform.position = this.transform.position + camOffset;
+    }
+    #endregion
+}
