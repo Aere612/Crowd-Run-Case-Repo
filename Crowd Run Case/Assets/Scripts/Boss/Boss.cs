422ce33a8a69631f1967d23d03032526c728235a
Ali Eren Aydın <Aere612@gmail.com>
2023-01-01 22:34:35 +0300

    bossss

--- Crowd Run Case/Assets/Scripts/Boss/Boss.cs
+++ Crowd Run Case/Assets/Scripts/Boss/Boss.cs
@@ -5,41 +5,55 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
     public int bossHP = 100;
-    public bool walk = true, kill = false;
+    public bool walk = true, kill = false ,canSwing=true;
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("player"))
+        if (other.CompareTag("Player"))
         {
+            GetComponent<Animator>().SetBool("isWalking",false);
             walk = false;
-            StartCoroutine(Swing());
+            if (canSwing)
+            {
+                canSwing = false;
+                StartCoroutine(Swing());
+            }
             if (kill)
             {
                 //kill players
                 kill = false;
+                //counteri azalt
                 bossHP -= 100;
             }
         }
     }
-    
     public void Update()
     {
+
         if (walk)
         {
-            //move forward
+            GetComponent<Animator>().SetBool("isWalking", true);
+            transform.Translate(new Vector3(0f, 0f, 0.01f));
         }
         if (bossHP <= 0)
         {
-            //death anim
-            //game win
-            //Gameobject.SetActive(false);
+            StartCoroutine(Death());
         }
     }
     IEnumerator Swing()
     {
         //swing anim start
-        //yield return new WaitForSeconds(anim time/2);
-        //kill=true;
+        yield return new WaitForSeconds(1f);
+        kill=true;
         //swing anim end
-        return null;
+        canSwing = true;
+        yield return null;
+    }
+    IEnumerator Death()
+    {
+        GetComponent<Animator>().SetBool("isDead", true);
+        yield return new WaitForSeconds(2f);
+        //win
+        Destroy(gameObject);
+        yield return null;
     }
 }
